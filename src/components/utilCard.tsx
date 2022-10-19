deb7ff6e5299870d58996ebd8a9b94b7ed82251e
iAverages <31937542+iAverages@users.noreply.github.com>
2022-10-19 18:31:08 +0100

    Update card hover style

--- src/components/utilCard.tsx
+++ src/components/utilCard.tsx
@@ -10,7 +10,7 @@ const UtilItemCard = ({ name, description, href }: UtilItemCardProps) => {
     return (
         <Link href={href}>
             <a className="m-auto mt-3 w-fit text-sm ">
-                <section className="flex flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 motion-safe:hover:scale-105">
+                <section className="flex flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 hover:shadow-selected  motion-safe:hover:scale-105">
                     <h2 className="text-lg text-gray-300">{name}</h2>
                     <p className="text-sm text-gray-600">{description}</p>
                 </section>
