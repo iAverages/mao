1488d24292f2d9440cb931a50f16aadb196ffa22
iAverages <31937542+iAverages@users.noreply.github.com>
2022-10-29 16:22:30 +0100

    Add daisyui + customize colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    daisyui: {
+        themes: [
+            {
+                plurple: {
+                    primary: "#9333ea",
+                    secondary: "#BF95F9",
+                    accent: "#FFB86B",
+                    neutral: "#414558",
+                    "base-100": "#272935",
+                    info: "#8BE8FD",
+                    success: "#4ade80",
+                    warning: "#F1FA89",
+                    error: "#FF5757",
+                },
+            },
+        ],
+    },
     theme: {
         extend: {
             boxShadow: {
@@ -9,5 +26,5 @@ module.exports = {
             },
         },
     },
-    plugins: [],
+    plugins: [require("daisyui")],
 };
