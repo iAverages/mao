e1c7e542c73224610d97d082a1778546a5dd7d33
iAverages <31937542+iAverages@users.noreply.github.com>
2022-10-29 16:25:03 +0100

    Change component styling to use daisyui

--- src/components/textarea.tsx
+++ src/components/textarea.tsx
@@ -1,31 +1,34 @@
-import { DetailedHTMLProps, TextareaHTMLAttributes } from "react";
+import { DetailedHTMLProps, TextareaHTMLAttributes, useRef } from "react";
+
+interface TextAreaProps
+    extends DetailedHTMLProps<
+        TextareaHTMLAttributes<HTMLTextAreaElement>,
+        HTMLTextAreaElement
+    > {
+    selectOnClick?: boolean;
+}
 
 const Textarea = ({
     className = "",
+    selectOnClick,
     children,
     ...props
-}: DetailedHTMLProps<
-    TextareaHTMLAttributes<HTMLTextAreaElement>,
-    HTMLTextAreaElement
->) => {
+}: TextAreaProps) => {
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+    const handleClick = () => {
+        if (selectOnClick) {
+            textAreaRef.current?.select();
+        }
+    };
+
     return (
         <textarea
+            ref={textAreaRef}
+            onClick={handleClick}
             className={
                 className +
-                ` w-full resize-none 
-                rounded-lg 
-                border 
-                border-none 
-                border-gray-600 
-                bg-neutral-700 
-                p-2.5 
-                text-sm 
-                text-white 
-                outline-none 
-                duration-100 
-                ease-in-out 
-                hover:shadow-selected-sm
-                focus:shadow-selected`
+                " w-full resize-none rounded-lg border border-none border-gray-600 bg-neutral p-2.5 text-sm text-white outline-none duration-100 ease-in-out hover:shadow-selected-sm focus:shadow-selected "
             }
             {...props}
         >
--- src/components/utilCard.tsx
+++ src/components/utilCard.tsx
@@ -9,10 +9,10 @@ type UtilItemCardProps = {
 const UtilItemCard = ({ name, description, href }: UtilItemCardProps) => {
     return (
         <Link href={href}>
-            <a className="m-auto mt-3 w-fit text-sm ">
+            <a className="  mt-3 flex flex-auto content-center items-center text-center text-sm">
                 <section className="flex flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 hover:shadow-selected  motion-safe:hover:scale-105">
                     <h2 className="text-lg text-gray-300">{name}</h2>
-                    <p className="text-sm text-gray-600">{description}</p>
+                    <p className="text-sm text-gray-400">{description}</p>
                 </section>
             </a>
         </Link>
