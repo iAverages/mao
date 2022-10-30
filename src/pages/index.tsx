59fccac75482354ba0ae47fa04a5e68f13bc0ddc
iAverages <31937542+iAverages@users.noreply.github.com>
2022-10-30 22:44:24 +0000

    Add new generate's to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,7 @@ const Home: NextPage = () => {
         <Layout>
             <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
                 <h1 className="text-5xl font-extrabold leading-normal text-gray-300 md:text-[5rem]">
-                    Useful <span className="text-purple-700">Utilities</span>{" "}
-                    App
+                    Useful <span className="text-purple-700">Utilities</span> App
                 </h1>
                 <p className="text-2xl text-gray-300">The tools included:</p>
                 <div className="mt-3 flex flex-row flex-wrap content-center items-stretch justify-around gap-4">
@@ -17,10 +16,12 @@ const Home: NextPage = () => {
                         description="Simple clientside base64 convertion tool"
                         href="/base64"
                     />
+                    <UtilItemCard name="JSON Pretty Print" description="Make ugly JSON pretty" href="/json" />
+                    <UtilItemCard name="Generate UUID v4" description="Quickly generate a UUID v4 string" href="/generate/uuid" />
                     <UtilItemCard
-                        name="JSON Pretty Print"
-                        description="Make ugly JSON pretty"
-                        href="/json"
+                        name="Generate Secure Secret"
+                        description="Quickly generate a secure string that can be used for secrets"
+                        href="/generate/secret"
                     />
                 </div>
             </main>
