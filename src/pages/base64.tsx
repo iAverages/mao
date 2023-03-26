d06a829c545fd314fc786ef207d6162331845c4d
iAverages <31937542+iAverages@users.noreply.github.com>
2023-03-26 22:54:21 +0100

    Fix base64 errors (#19)

--- src/pages/base64.tsx
+++ src/pages/base64.tsx
@@ -3,13 +3,24 @@ import Layout from "../components/layout";
 import Textarea from "../components/textarea";
 import toBase64 from "../utils/toBase64";
 
+const EDITING = ["text", "base64"] as const;
+
 const Base64 = () => {
     const [text, setText] = useState("");
     const [converted, setConverted] = useState(text);
     const [editingText, setEditingText] = useState(false);
-    const [editingImage, setEditingImage] = useState(false);
+    const [editingState, setEditingState] = useState<typeof EDITING[number]>(editingText ? "text" : "base64");
 
-    const hanleConvertToImage = () => {
+    const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files;
+        if (!files || files.length == 0) return;
+        const file = files[0]!;
+        setText("");
+        setEditingText(true);
+        setConverted(await toBase64(file));
+    };
+
+    const handleConvertToImage = () => {
         const link = document.createElement("a");
         link.href = "data:;base64," + converted;
         link.download = `download-${Date.now()}`;
@@ -17,25 +28,33 @@ const Base64 = () => {
         link.remove();
     };
 
+    const encode = () => {
+        try {
+            setConverted(btoa(text));
+        } catch (error) {
+            setConverted("Invalid text");
+        }
+    };
+
+    const decode = () => {
+        try {
+            setText(atob(converted));
+        } catch (error) {
+            setText("Invalid text");
+        }
+    };
+
     useEffect(() => {
-        setEditingText(true);
+        if (editingState == "text") {
+            encode();
+        }
     }, [text]);
 
     useEffect(() => {
-        setEditingText(false);
+        if (editingState == "base64") {
+            decode();
+        }
     }, [converted]);
-
-    useEffect(() => {
-        editingText && setConverted(btoa(text));
-    }, [text, editingText]);
-
-    useEffect(() => {
-        !editingText && !editingImage && setText(atob(converted));
-    }, [converted, editingText, editingImage]);
-
-    useEffect(() => {
-        editingImage && setConverted(btoa(text));
-    }, [text, editingImage]);
 
     return (
         <Layout title="Base64 text conversion">
@@ -45,26 +64,15 @@ const Base64 = () => {
                     <h2>Upload image</h2>
                     <div className="flex gap-3 py-3">
                         <input
-                            className="file-input file-input-bordered w-full max-w-xs"
+                            className="file-input-bordered file-input w-full max-w-xs"
                             type="file"
-                            onChange={async (e) => {
-                                const files = e.target.files;
-                                if (!files || files.length == 0) return;
-                                const file = files[0];
-                                if (file) {
-                                    setEditingText(true);
-                                    setEditingImage(true);
-                                    setConverted(await toBase64(file));
-                                } else {
-                                    console.log("no file");
-                                }
-                            }}
+                            onChange={handleFileUpload}
                         />
                         <div
                             className="tooltip tooltip-right"
                             data-tip="Converts Text within the base64 conversion box to an image"
                         >
-                            <label className="btn btn-primary" htmlFor="image-converstion" onClick={hanleConvertToImage}>
+                            <label className="btn-primary btn" htmlFor="image-converstion" onClick={handleConvertToImage}>
                                 Convert Text to Image
                             </label>
                         </div>
@@ -75,14 +83,25 @@ const Base64 = () => {
                         <label className="text-lg" htmlFor="text">
                             Text
                         </label>
-                        <Textarea onChange={(e) => setText(e.target.value)} value={text} id="text" className="flex-1" />
+                        <Textarea
+                            onChange={(e) => {
+                                setEditingState("text");
+                                setText(e.target.value);
+                            }}
+                            value={text}
+                            id="text"
+                            className="flex-1"
+                        />
                     </div>
                     <div className="flex h-full w-full flex-col">
                         <label className="text-lg" htmlFor="converted">
                             Base64 Conversion
                         </label>
                         <Textarea
-                            onChange={(e) => setConverted(e.target.value)}
+                            onChange={(e) => {
+                                setEditingState("base64");
+                                setConverted(e.target.value);
+                            }}
                             value={converted}
                             id="converted"
                             className="flex-1"
