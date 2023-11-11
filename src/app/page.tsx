8e64bd578be268b9aefedce9eb18b34fb781f1a3
iAverages <31937542+iAverages@users.noreply.github.com>
2023-11-11 22:41:15 +0000

    Change homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { H1, H3 } from "~/components/typography";
+
 export default function Home() {
-    return <div>Yippie</div>;
+    return (
+        <div className={"flex flex-col gap-3"}>
+            <H1>Mao</H1>
+            <H3>Select one of the utilities from the menu.</H3>
+        </div>
+    );
 }
